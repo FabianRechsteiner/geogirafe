7a84fe3a4ebe558f434b041444bde416a8f1965e
Guillaume <guillaume@paloo.fr>
2022-10-04 21:55:38 +0200

    Add some styling

--- buildtools/prebuild.sh
+++ buildtools/prebuild.sh
@@ -8,6 +8,3 @@ cd ..
 # Copy CSS styles for Libraries (are not included in css bundle)
 mkdir -p src/static/lib/
 cp node_modules/ol/ol.css src/static/lib/
-cp node_modules/@fortawesome/fontawesome-free/css/all.min.css src/static/lib/
-#cp node_modules/@fortawesome/fontawesome-free/js/all.min.js src/static/lib/
-cp -r node_modules/@fortawesome/fontawesome-free/webfonts src/static/
