a700524943e3bc0b052ee9ddae3d666589b157b0
Guillaume REMY <guillaume.remy@bs.ch>
2022-10-06 17:18:29 +0200

    Configure baseurl to make it work in gitlab pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 // https://v2.vitejs.dev/config/
 export default defineConfig({
     root: './src',
-    base: '/',
+    base: '/gg_viewer/',
     publicDir: './static',
     build: {
         outDir: '../public',
