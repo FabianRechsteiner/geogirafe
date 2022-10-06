2157293c240d6b3391998edc25bfdc5cfa508c7a
Guillaume REMY <guillaume.remy@bs.ch>
2022-10-06 17:37:21 +0200

    Revert the last change: solve it with a DNS alias

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 // https://v2.vitejs.dev/config/
 export default defineConfig({
     root: './src',
-    base: '/gg_viewer/',
+    base: '/',
     publicDir: './static',
     build: {
         outDir: '../public',
