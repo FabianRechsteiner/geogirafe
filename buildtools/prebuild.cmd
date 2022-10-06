1acb3a55e9edd53f05ce96324a2bd7f2a513efe7
Guillaume REMY <guillaume.remy@bs.ch>
2022-10-06 14:29:51 +0200

    Add prebuild script for Windows

---
+++ buildtools/prebuild.cmd
@@ -0,0 +1,9 @@
+@echo off
+REM Copy HTML Templates
+cd src
+xcopy components\*.html static\components\ /S /Y
+cd .. 
+
+REM Copy CSS styles for Libraries (are not included in css bundle)
+if not exist src\static\lib mkdir src\static\lib
+xcopy node_modules\ol\ol.css src\static\components\ol\ /Y
