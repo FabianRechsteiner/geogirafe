4345f4e0d9e4f0f11bd6fa49cf611e5e5245bc43
Guillaume <guillaume@paloo.fr>
2022-12-05 20:37:19 +0100

    style search results

--- src/components/search/component.js
+++ src/components/search/component.js
@@ -4,6 +4,10 @@ class SearchComponent extends GirafeHTMLElement {
 
   static #template = null;
   searchUrl = null;
+  searchBox = null;
+  resultsBox = null;
+  ignoreBlur = false;
+
   searchTermPlaceholder = '###SEARCHTERM###';
   initialSearchBoxHeight = this.convertRemToPixels(2.5);
   
@@ -30,15 +34,30 @@ class SearchComponent extends GirafeHTMLElement {
     this.shadow.appendChild(SearchComponent.#template.content.cloneNode(true));
 
     // Get default height of searchBox
-    const searchBox = this.shadow.querySelector('#searchbox');
+    this.searchBox = this.shadow.querySelector('#searchbox');
+    this.resultsBox = this.shadow.querySelector('#results');
   }
 
   registerEvents() {
     //window.addEventListener(GeoEvents.TreeView, (e) => this.onTreeViewEvent(e.detail));
-    const searchbox = this.shadow.querySelector('#search');
-    searchbox.addEventListener('input', (e) => this.doSearch(this, e));
+    this.searchBox.addEventListener('input', (e) => this.doSearch(this, e));
+    this.searchBox.addEventListener('focusin', (e) => this.onFocusIn(e));
+    this.searchBox.addEventListener('focusout', (e) => this.onFocusOut(e));
   }
 
+  onFocusIn(e) {
+    this.ignoreBlur = false;
+    this.resultsBox.style.display = 'block';
+    this.setSearchBoxHeight(this.initialSearchBoxHeight + this.resultsBox.offsetHeight + 20);
+  }
+
+  onFocusOut(e) {
+    if (!this.ignoreBlur) {
+      this.resultsBox.style.display = 'none';
+      this.setSearchBoxHeight(this.initialSearchBoxHeight);
+    }
+  }
+  
   connectedCallback() {
     this.loadTemplate().then(() => {
       this.render();
@@ -47,8 +66,7 @@ class SearchComponent extends GirafeHTMLElement {
   }
 
   clearSearch() {
-    const resultsBox = this.shadow.querySelector('#results');
-    resultsBox.innerHTML = '';
+    this.resultsBox.innerHTML = '';
     this.setSearchBoxHeight(this.initialSearchBoxHeight);
   }
 
@@ -66,25 +84,84 @@ class SearchComponent extends GirafeHTMLElement {
   }
 
   displayResults(results) {
-    this.clearSearch();
-    if (results.length === 0)
-      return;
 
-    const resultsBox = this.shadow.querySelector('#results');
-    results.forEach(result => {
-      const span = document.createElement(`span`);
-      span.textContent = result.label;
-      span.className = 'result';
-      span.style.cursor = 'pointer';
-      span.onclick = (e) => this.onSelect(e);
-      resultsBox.appendChild(span);
+    // First, group the results
+    const groupedResults = {}
+    results.features.forEach(result => {
+      const type = result.properties.layer_name;
+
+      let resultList = null;
+      if (type in groupedResults) {
+        resultList = groupedResults[type];
+      }
+      else {
+        resultList = [];
+        groupedResults[type] = resultList;
+      }
+
+      resultList.push(result);
     });
-    this.setSearchBoxHeight(this.initialSearchBoxHeight + resultsBox.offsetHeight + 20);
+
+    // Then, display the results by group
+    this.clearSearch();
+    for (const type in groupedResults) {
+
+      // Create a title
+      const title = document.createElement('div');
+      title.className = 'title';
+
+      const icon = document.createElement('i');
+      icon.className = this.getIconClassName(type);
+      title.appendChild(icon);
+      
+      const titleText = document.createElement('span');
+      titleText.innerHTML = type;
+      title.appendChild(titleText);
+
+      this.resultsBox.appendChild(title);
+
+      // Create results
+      groupedResults[type].forEach(r => {
+        const result = document.createElement('div');
+        result.className = 'result';
+        result.onmousedown = () => { this.ignoreBlur = true };
+        result.onclick = (e) => { this.ignoreBlur = false; alert(e); };
+    
+
+        const text = document.createElement('span');
+        text.innerHTML = r.properties.label;
+        result.appendChild(text);
+
+        this.resultsBox.appendChild(result);
+      });
+    }
+
+    this.setSearchBoxHeight(this.initialSearchBoxHeight + this.resultsBox.offsetHeight + 20);
+  }
+
+  getIconClassName(type) {
+    switch(type) {
+      case 'Adresse':
+        return 'fa-solid fa-location-dot';
+      case 'Basel Info (BI)':
+        return 'fa-solid fa-map-location-dot';
+      case 'Baumnummer öffentlicher Baumkataster':
+        return 'fa-solid fa-tree';
+      case 'Entsorgungsstellen':
+        return 'fa-solid fa-recycle';
+      case 'Haltestelle öffentlicher Verkehr':
+        return 'fa-solid fa-train-subway';
+      case 'Group':
+        return 'fa-solid fa-layer-group';
+      case 'Layer':
+        return 'fa-solid fa-map';
+      default:
+        return 'fa-solid fa-globe';
+    }
   }
 
   setSearchBoxHeight(height) {
-    const searchBox = this.shadow.querySelector('#searchbox');
-    searchBox.style.height = height + 'px';
+    this.searchBox.style.height = height + 'px';
   }
 
   onSelect(e) {
