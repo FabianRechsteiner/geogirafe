a14a2d28fd89d6454285bdfec2647d356ea2c6b4
Guillaume <guillaume@paloo.fr>
2023-02-18 14:33:51 +0100

    Add ScaleComponent

---
+++ src/components/scale/component.js
@@ -0,0 +1,42 @@
+import GeoEvents from '../../models/events.js';
+import GirafeHTMLElement from '../../base/GirafeHTMLElement.js';
+
+class ScaleComponent extends GirafeHTMLElement {
+
+  scaleSpan = null;
+  locale = null;
+
+  constructor() {
+    super('scale');
+  }
+
+  render() {
+    super.render();
+    this.scaleSpan = this.shadow.querySelector('#scale');
+
+    this.locale = this.configManager.Config.general.locale;
+  }
+
+  registerEvents() {
+    window.addEventListener(GeoEvents.Map, (e) => this.onMapEvent(e.detail));
+  }
+
+  onMapEvent(details) {
+    if (details.action === 'resolutionChanged') {
+      this.scaleSpan.innerHTML ='1:' + Math.floor(details.scale).toLocaleString(this.locale);
+    }
+  }
+
+  connectedCallback() {
+    this.loadTemplate().then(() => {
+      this.render();
+      super.translate();
+      this.registerEvents();
+      //super.initialized();
+    });
+  }
+}
+
+customElements.define('girafe-scale', ScaleComponent);
+
+export default ScaleComponent;
--- src/initialize.js
+++ src/initialize.js
@@ -12,6 +12,7 @@ import BasemapComponent from './components/basemap/component.js';
 import ThemeComponent from './components/themes/component.js';
 import LanguageComponent from './components/language/component.js';
 import ButtonComponent from './components/button/component.js';
+import ScaleComponent from './components/scale/component.js';
 import MenuButtonComponent from './components/menubutton/component.js';
 import RedliningComponent from './components/redlining/component.js';
 import PrintComponent from './components/print/component.js';
