5f2efed2c88c47102cde2b7be418995445e1042d
Guillaume <guillaume@paloo.fr>
2023-05-21 17:23:29 +0200

    Add demo for Liechtenstein

--- buildtools/configure-demo.sh
+++ buildtools/configure-demo.sh
@@ -15,5 +15,18 @@ then
     curl "https://edit.geo.gr.ch/static-ngeo/build/de.json" --silent --output $OUTPUTDIR/de.json
     curl "https://edit.geo.gr.ch/printproxy/capabilities.json" --silent --output $OUTPUTDIR/capabilities.json
 
+elif [ "$1" = 'lie' ];
+then
+    echo "Preparing environment LIE..."
+    cp demo/config.lie.json src/static/config.json
+    curl "https://map.geo.llv.li/themes?background=background&interface=desktop" --silent --output $OUTPUTDIR/themes.json
+    curl "https://map.geo.llv.li/static/X/de.json" --silent --output $OUTPUTDIR/de.json
+    curl "https://map.geo.llv.li/printproxy/capabilities.json" --silent --output $OUTPUTDIR/capabilities.json
+
+else
+    echo "Possible options environments: ['sitn', 'geogr', 'lie']"
+    echo "Usage example: ./configure-demo sitn"
+    echo "Usage example with npm: npm run configure-demo sitn"
+
 fi
 
