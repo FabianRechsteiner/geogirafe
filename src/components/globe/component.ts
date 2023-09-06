6223dd66e62af96eb2734180bf716b14b34728be
Rémi Bovard <remi.bovard@vevey.ch>
2023-09-06 18:08:16 +0200

    Remove commented code

--- src/components/globe/component.ts
+++ src/components/globe/component.ts
@@ -24,31 +24,10 @@ class GlobeComponent extends GirafeHTMLElement {
     this.#menuButton = this.shadow.querySelector('#menu-button')!;
   }
 
-  registerEvents() {
-    //this.stateManager.subscribe('globe.display', () => this.onGlobeChanged());
-  }
-
-  onGlobeChanged() {
-    // TODO REG : Do we want to do this?
-    /*if (this.state.globe.display === 'full') {
-      // Full screen globe has been enabled
-      this.menuButton.setText('3D');
-    }
-    else if (this.state.globe.display === 'side') {
-      // Side by side has been enabled
-      this.menuButton.setText('Both');
-    }
-    else {
-      // 3d map is not visible
-      this.menuButton.setText('2D');
-    }*/
-  }
-
   connectedCallback() {
     this.loadConfig().then(() => {
       this.render();
       super.girafeTranslate();
-      this.registerEvents();
     });
   }
 }
