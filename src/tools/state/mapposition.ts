104b7ac172490118de1bced42d6ae78f3461c79e
Laurent Lienher <laurent.lienher@camptocamp.com>
2025-02-18 09:17:09 +0100

    Add map crosshair in url parameter

--- src/tools/state/mapposition.ts
+++ src/tools/state/mapposition.ts
@@ -5,6 +5,7 @@ class MapPosition {
   zoom: number = 0;
   resolution: number = 100; /* dummy default value because it should never be null. It will be recalculated when the map will be created */
   scale: number = 0;
+  crosshair: boolean = false;
 
   get isValid() {
     if (Number.isNaN(this.resolution)) {
@@ -21,7 +22,6 @@ class MapPosition {
 
 export default MapPosition;
 
-
 /**
  * Extracts map position details `map_x`, `map_y`, and `map_zoom` from the current URL's query parameters.
  * @returns {MapPosition | undefined} A `MapPosition` instance if valid parameters are found, otherwise undefined.
@@ -31,10 +31,12 @@ export const parseMapPositionFromUrl = (): MapPosition | undefined => {
   const mapX = url.searchParams.get('map_x');
   const mapY = url.searchParams.get('map_y');
   const mapZoom = url.searchParams.get('map_zoom');
+  const crosshair = url.searchParams.get('map_crosshair');
   if (mapX && mapY && mapZoom) {
     const newPosition = new MapPosition();
     newPosition.center = [parseFloat(mapX), parseFloat(mapY)];
     newPosition.zoom = parseFloat(mapZoom);
+    newPosition.crosshair = crosshair === 'true';
     return newPosition.isValid ? newPosition : undefined;
   }
   return undefined;
