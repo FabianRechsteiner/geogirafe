1ca7c020c848cb76e3e6e1618a5f51074eed975d
Guillaume REMY <guillaume.remy@bs.ch>
2025-07-10 10:57:08 +0200

    Simplify the configure-demo scripts

--- buildtools/configure-demo.cmd
+++ buildtools/configure-demo.cmd
@@ -1,127 +1,35 @@
 @echo off
 
-set APPDIR="public"
-set OUTPUTDIR="%APPDIR%\Mock"
+set "APPDIR=public"
+set "OUTPUTDIR=%APPDIR%\Mock"
 if not exist %OUTPUTDIR% mkdir %OUTPUTDIR%
 
-if "%1"=="c2c" (
-    echo "Preparing environment CAMPTOCAMP..."
-    copy demo\config.c2c.json %APPDIR%\config.json /Y
-    goto :end
+set "name=%1"
+echo You can get help with the argument help
+if "%name%"=="help" (
+    echo "Usage: ./configure-demo-win <environment>"
+    echo "Possible environments: ['c2c', 'experimental', 'cartolacote', 'cartoriviera', 'cjl', 'geocommunes', 'geogr', 'georama', 'jura', 'lausanne', 'lie', 'mapbs', 'mapnv', 'schwyz', 'sigip', 'sitn', 'ticino']"
+    echo "Usage example: ./configure-demo-win mapbs"
+    echo "Usage example with npm: npm run configure-demo-win mapbs"
+    exit
 )
 
-
-if "%1"=="experimental" (
-    echo "Preparing environment EXPERIMENTAL..."
-    copy demo\config.experimental.json %APPDIR%\config.json /Y
-    goto :end
+echo Preparing environment for %name%...
+copy /Y "demo\config.%name%.json" "%APPDIR%\config.json"
+if exist "demo\config.%name%.mobile.json" (
+    copy /Y "demo\config.%name%.mobile.json" "%APPDIR%\config.mobile.json"
 )
 
-
-if "%1"=="cartolacote" (
-    echo "Preparing environment CARTOLACOTE..."
-    copy demo\config.cartolacote.json %APPDIR%\config.json /Y
-    goto :end
-)
-
-if "%1"=="cartoriviera" (
-    echo "Preparing environment CARTORIVIERA..."
-    copy demo\config.cartoriviera.json %APPDIR%\config.json /Y
-    copy demo\config.cartoriviera.mobile.json %APPDIR%\config.mobile.json /Y
-    goto :end
-)
-
-if "%1"=="cjl" (
-    echo "Preparing environment CARTOJURALEMAN..."
-    copy demo\config.cjl.json %APPDIR%\config.json /Y
-    goto :end
-)
-
-if "%1"=="geocommunes" (
-    echo "Preparing environment GEOCOMMUNES..."
-    copy demo\config.geocommunes.json %APPDIR%\config.json /Y
-    goto :end
-)
-
-if "%1"=="geogr" (
-    echo "Preparing environment GEOGR..."
-    copy demo\config.geogr.json %APPDIR%\config.json /Y
-    goto :end
-)
-
-if "%1"=="georama" (
-    echo "Preparing environment GEORAMA..."
-    copy demo\config.georama.json %APPDIR%\config.json /Y
-    goto :end
-)
-
-if "%1"=="jura" (
-    echo "Preparing environment JURA..."
-    copy demo\config.jura.json %APPDIR%\config.json /Y
-    goto :end
-)
-
-if "%1"=="lausanne" (
-    echo "Preparing environment LAUSANNE..."
-    copy demo\config.lausanne.json %APPDIR%\config.json /Y
+if "%name%"=="lausanne" (
     curl "https://map.lausanne.ch/themes?background=background&interface=desktop" --output %OUTPUTDIR%\themes.json
     curl "https://map.lausanne.ch/static/dummy/fr.json" --output %OUTPUTDIR%\fr.json
-    goto :end
-)
-
-if "%1"=="lie" (
-    echo "Preparing environment LIE..."
-    copy demo\config.lie.json %APPDIR%\config.json /Y
+) else if "%name%"=="lie" (
     curl "https://map.geo.llv.li/themes?background=background&interface=desktop" --output %OUTPUTDIR%\themes.json
     curl "https://map.geo.llv.li/static/dummy/de.json" --output %OUTPUTDIR%\de.json
-    goto :end
-)
-
-if "%1"=="mapbs" (
-    echo "Preparing environment MAPBS..."
-    copy demo\config.mapbs.json %APPDIR%\config.json /Y
-    copy demo\config.mapbs.mobile.json %APPDIR%\config.mobile.json /Y
-    goto :end
-)
-
-if "%1"=="mapnv" (
-    echo "Preparing environment MAPNV..."
-    copy demo\config.mapnv.json %APPDIR%\config.json /Y
+) else if "%name%"=="mapnv" (
     curl "https://mapnv.ch/themes?background=background&interface=desktop" --output %OUTPUTDIR%\themes.json
     curl "https://mapnv.ch/static/dummy/fr.json" --output %OUTPUTDIR%\fr.json
-    goto :end
-)
-
-if "%1"=="schwyz" (
-    echo "Preparing environment SCHWYZ..."
-    copy demo\config.schwyz.json %APPDIR%\config.json /Y
-    goto :end
-)
-
-if "%1"=="sigip" (
-    echo "Preparing environment SIGIP..."
-    copy demo\config.sigip.json %APPDIR%\config.json /Y
-    goto :end
-)
-
-if "%1"=="sitn" (
-    echo "Preparing environment SITN..."
-    copy demo\config.sitn.json %APPDIR%\config.json /Y
-    copy demo\config.sitn.mobile.json %APPDIR%\config.mobile.json /Y
-    goto :end
-)
-
-if "%1"=="ticino" (
-    echo "Preparing environment TICINO..."
-    copy demo\config.ticino.json %APPDIR%\config.json /Y
+) else if "%name%"=="ticino" (
     curl "https://map.geo.ti.ch/themes?background=background&interface=desktop" --output %OUTPUTDIR%\themes.json
     curl "https://map.geo.ti.ch/static/dummy/it.json" --output %OUTPUTDIR%\it.json
-    goto :end
 )
-
-echo "Usage: ./configure-demo-win <environment>"
-echo "Possible environments: ['c2c', 'experimental', 'cartolacote', 'cartoriviera', 'cjl', 'geocommunes', 'geogr', 'georama', 'jura', 'lausanne', 'lie', 'mapbs', 'mapnv', 'schwyz', 'sigip', 'sitn', 'ticino']"
-echo "Usage example: ./configure-demo-win mapbs"
-echo "Usage example with npm: npm run configure-demo-win mapbs"
-
-:end
--- buildtools/configure-demo.sh
+++ buildtools/configure-demo.sh
@@ -8,105 +8,42 @@ fi
 MOCKDIR="$APPDIR/Mock"
 mkdir -p "$MOCKDIR"
 
-if [ "$1" = 'c2c' ];
+name=$1
+echo "You can get help with the argument help"
+if [ "$name" = 'help' ];
 then
-    echo "Preparing environment CAMPTOCAMP..."
-    cp demo/config.$1.json $APPDIR/config.json
-elif [ "$1" = 'experimental' ];
-then
-    echo "Preparing environment EXPERIMENTAL..."
-    cp demo/config.$1.json $APPDIR/config.json
-elif [ "$1" = 'cartolacote' ];
-then
-    echo "Preparing environment CARTOLACOTE..."
-    cp demo/config.$1.json $APPDIR/config.json
-
-elif [ "$1" = 'cartoriviera' ];
-then
-    echo "Preparing environment CARTORIVIERA..."
-    cp demo/config.$1.json $APPDIR/config.json
-    cp demo/config.$1.mobile.json $APPDIR/config.mobile.json
-    echo "Cannot create Mock objects for cartoriviera, the following commands are blocked when executed from AWS or GitLab Pipeline"
-
-elif [ "$1" = 'cjl' ];
-then
-    echo "Preparing environment CARTOJURALEMAN..."
-    cp demo/config.$1.json $APPDIR/config.json
-
-elif [ "$1" = 'geocommunes' ];
-then
-    echo "Preparing environment GEOCOMMUNES..."
-    cp demo/config.$1.json $APPDIR/config.json
-
-elif [ "$1" = 'geogr' ];
-then
-    echo "Preparing environment GEOGR..."
-    cp demo/config.$1.json $APPDIR/config.json
-
-elif [ "$1" = 'georama' ];
-then
-    echo "Preparing environment GEORAMA..."
-    cp demo/config.$1.json $APPDIR/config.json
-
-elif [ "$1" = 'jura' ];
-then
-    echo "Preparing environment JURA..."
-    cp demo/config.$1.json $APPDIR/config.json
-
-elif [ "$1" = 'lausanne' ];
-then
-    echo "Preparing environment LAUSANNE..."
-    cp demo/config.$1.json $APPDIR/config.json
-    curl "https://map.lausanne.ch/themes?background=background&interface=desktop" --silent --output $MOCKDIR/themes.json
-    curl "https://map.lausanne.ch/static/dummy/fr.json" --silent --output $MOCKDIR/fr.json
-
-elif [ "$1" = 'lie' ];
-then
-    echo "Preparing environment LIE..."
-    cp demo/config.$1.json $APPDIR/config.json
-    curl "https://map.geo.llv.li/themes?background=background&interface=desktop" --silent --output $MOCKDIR/themes.json
-    curl "https://map.geo.llv.li/static/dummy/de.json" --silent --output $MOCKDIR/de.json
-
-elif [ "$1" = 'mapbs' ];
-then
-    echo "Preparing environment MAPBS..."
-    cp demo/config.$1.json $APPDIR/config.json
-    cp demo/config.$1.mobile.json $APPDIR/config.mobile.json
-
-elif [ "$1" = 'mapnv' ];
-then
-    echo "Preparing environment MAPNV..."
-    cp demo/config.$1.json $APPDIR/config.json
-    curl "https://mapnv.ch/themes?background=background&interface=desktop" --silent --output $MOCKDIR/themes.json
-    curl "https://mapnv.ch/static/dummy/fr.json" --silent --output $MOCKDIR/fr.json
-
-elif [ "$1" = 'schwyz' ];
-then
-    echo "Preparing environment SCHWYZ..."
-    cp demo/config.$1.json $APPDIR/config.json
-
-elif [ "$1" = 'sigip' ];
-then
-    echo "Preparing environment SIGIP..."
-    cp demo/config.$1.json $APPDIR/config.json
-
-elif [ "$1" = 'sitn' ];
-then
-    echo "Preparing environment SITN..."
-    cp demo/config.$1.json $APPDIR/config.json
-    cp demo/config.$1.mobile.json $APPDIR/config.mobile.json
-
-elif [ "$1" = 'ticino' ];
-then
-    echo "Preparing environment TICINO..."
-    cp demo/config.$1.json $APPDIR/config.json
-    curl "https://map.geo.ti.ch/themes?background=background&interface=desktop" --silent --output $MOCKDIR/themes.json
-    curl "https://map.geo.ti.ch/static/dummy/it.json" --silent --output $MOCKDIR/it.json
-
-else
     echo "Usage: ./configure-demo <environment>"
     echo "Possible environments: ['c2c', 'experimental', 'cartolacote', 'cartoriviera', 'cjl', 'geogr', 'georama', 'jura', 'lausanne', 'lie', 'mapbs', 'mapnv', 'schwyz', 'sigip', 'sitn', 'ticino']"
     echo "Usage example: ./configure-demo mapbs"
     echo "Usage example with npm: npm run configure-demo mapbs"
+    exit
+fi
+
+echo "Preparing environment ${name^^}..."
+cp "demo/config.$name.json" "$APPDIR/config.json"
+if [ -f "demo/config.$name.mobile.json" ]; then
+    cp "demo/config.$name.mobile.json" "$APPDIR/config.mobile.json"
+fi
+
+# Special cases for Mocking themes and translations (still CORS errors from the backend)
+if [ "$name" = 'lausanne' ];
+then
+    curl "https://map.lausanne.ch/themes?background=background&interface=desktop" --silent --output $MOCKDIR/themes.json
+    curl "https://map.lausanne.ch/static/dummy/fr.json" --silent --output $MOCKDIR/fr.json
+
+elif [ "$name" = 'lie' ];
+then
+    curl "https://map.geo.llv.li/themes?background=background&interface=desktop" --silent --output $MOCKDIR/themes.json
+    curl "https://map.geo.llv.li/static/dummy/de.json" --silent --output $MOCKDIR/de.json
+
+elif [ "$name" = 'mapnv' ];
+then
+    curl "https://mapnv.ch/themes?background=background&interface=desktop" --silent --output $MOCKDIR/themes.json
+    curl "https://mapnv.ch/static/dummy/fr.json" --silent --output $MOCKDIR/fr.json
+
+elif [ "$name" = 'ticino' ];
+then
+    curl "https://map.geo.ti.ch/themes?background=background&interface=desktop" --silent --output $MOCKDIR/themes.json
+    curl "https://map.geo.ti.ch/static/dummy/it.json" --silent --output $MOCKDIR/it.json
 
 fi
