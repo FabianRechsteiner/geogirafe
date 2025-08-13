16b87676580233eadb7a02c62eb9b332c65de074
Pi Moll <patricia@opengis.ch>
2025-08-13 13:46:33 +0200

    Edit module - Introduce OIDC workflow for georama

--- src/tools/ogcapi/demolayers.ts
+++ src/tools/ogcapi/demolayers.ts
@@ -5,9 +5,8 @@ export const DEMO_LAYERS: Record<string, any> = {
   GEORAMA: {
     url: 'https://demo.georama.io/features',
     name: 'Georama Rivers',
-    collectionId: '60c4f0d6-2d57-4db1-bf7e-0f0f8f9c1e36',
+    collectionId: 'cb14a0d9-016b-454a-84a5-475340f0a433',
     geometryType: 'MultiLineString',
-    credentials: 'editor:6YPhizVs4BZPvm', // Credentials for demo env, not a risk
     serverType: 'georama',
     server: new ServerOgc('GEORAMA', {
       url: 'https://demo.georama.io/features',
@@ -24,7 +23,6 @@ export const DEMO_LAYERS: Record<string, any> = {
     name: 'GeoMapFish Points',
     collectionId: 'points',
     geometryType: 'Point',
-    credentials: undefined,
     serverType: 'gmf',
     server: new ServerOgc('gmf', {
       url: 'https://geomapfish-demo-2-9.camptocamp.com/mapserv_proxy/QGIS_Server/wfs3',
