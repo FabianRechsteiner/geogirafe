91dbc4f30bf0c186d09cefc5ada5ab4b55d2e595
Stéphane Malta e Sousa <stephane.maltaesousa@ne.ch>
2025-11-20 07:09:53 +0000

    Redirect mobile

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { redirectTo, SplashScreen } from './main.tools';
 import GeoGirafeApp from './tools/app/geogirafeapp';
 
-// Redirect to mobile interface if we are on mobile
-if (navigator.userAgent.includes('iPhone') || navigator.userAgent.includes('Android')) {
+const shouldSkipRedirect = new URLSearchParams(globalThis.location.search).has('no_redirect');
+
+// Redirect to mobile interface if we are on mobile and no_redirect is not set
+if (!shouldSkipRedirect && (navigator.userAgent.includes('iPhone') || navigator.userAgent.includes('Android'))) {
   const redirectUrl = document.querySelector('meta[name=redirect-url]')?.getAttribute('content');
   if (redirectUrl) {
     redirectTo(redirectUrl);
