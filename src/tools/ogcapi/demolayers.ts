ab3c44d4425dfcccf02e05e65c51c9db9a9e36d4
maltaesousa <stephane.maltaesousa@ne.ch>
2026-04-10 09:06:40 +0200

    add sitn demo edit layers

--- src/tools/ogcapi/demolayers.ts
+++ src/tools/ogcapi/demolayers.ts
@@ -2,6 +2,18 @@
 // POC demo variables
 import ServerOgc from '../../models/serverogc';
 
+const SITN_SERVER = new ServerOgc('gmf', {
+  url: 'https://sitn.ne.ch/mapserv_proxy/qgisserver/wfs3',
+  wfsSupport: false,
+  urlWfs: '',
+  oapifSupport: true,
+  urlOapif: 'https://sitn.ne.ch/mapserv_proxy/qgisserver/wfs3',
+  // @ts-expect-error Defining a new type of server here to be able to catch some miss-configuration
+  //  of the GMF demo server, see ogcapifeaturesclientgmf.ts
+  type: 'gmf',
+  imageType: ''
+});
+
 export const DEMO_LAYERS: Record<string, any> = {
   GEORAMA: {
     url: 'https://demo.georama.io/features/api',
@@ -40,5 +52,50 @@ export const DEMO_LAYERS: Record<string, any> = {
       type: 'gmf',
       imageType: ''
     })
+  },
+  SITN_Point: {
+    url: 'https://sitn.ne.ch/mapserv_proxy/qgisserver/wfs3',
+    displayName: 'SITN point',
+    collectionId: 'demo01_edit_point',
+    collectionTitle: null,
+    geometryType: 'Point',
+    serverType: 'gmf',
+    server: SITN_SERVER
+  },
+  SITN_LineString: {
+    url: 'https://sitn.ne.ch/mapserv_proxy/qgisserver/wfs3',
+    displayName: 'SITN line',
+    collectionId: 'demo02_edit_line',
+    collectionTitle: null,
+    geometryType: 'LineString',
+    serverType: 'gmf',
+    server: SITN_SERVER
+  },
+  SITN_Polygon: {
+    url: 'https://sitn.ne.ch/mapserv_proxy/qgisserver/wfs3',
+    displayName: 'SITN polygon',
+    collectionId: 'demo03_edit_polygon',
+    collectionTitle: null,
+    geometryType: 'Polygon',
+    serverType: 'gmf',
+    server: SITN_SERVER
+  },
+  SITN_MultiLineString: {
+    url: 'https://sitn.ne.ch/mapserv_proxy/qgisserver/wfs3',
+    displayName: 'SITN multiline',
+    collectionId: 'demo04_edit_multiline',
+    collectionTitle: null,
+    geometryType: 'MultiLineString',
+    serverType: 'gmf',
+    server: SITN_SERVER
+  },
+  SITN_MultiPolygon: {
+    url: 'https://sitn.ne.ch/mapserv_proxy/qgisserver/wfs3',
+    displayName: 'SITN multipolygon',
+    collectionId: 'demo05_edit_multipolygon',
+    collectionTitle: null,
+    geometryType: 'MultiPolygon',
+    serverType: 'gmf',
+    server: SITN_SERVER
   }
 };
--- src/tools/ogcapi/ogcapifeaturesclientgmf.ts
+++ src/tools/ogcapi/ogcapifeaturesclientgmf.ts
@@ -19,17 +19,19 @@ export default class OgcApiFeaturesClientGmf extends OgcApiFeaturesClient {
     const items = await this.getItems(collectionId, undefined, undefined, 1);
     const item: Feature = items[0];
     const properties: Record<string, OapifPropertySchema> = Object.fromEntries(
-      Object.entries(item.getProperties())
-        .filter(([key, _value]) => ['fid', 'name', 'type'].includes(key))
-        .map(([key, value]) => [key, { type: typeof value }])
+      Object.entries(item.getProperties()).map(([key, value]) => [key, { type: typeof value }])
     );
     // Add geometry information
     properties['geometry'] = {
       'format': `geometry-${item.getGeometry()!.getType().toLowerCase()}`,
       'x-ogc-role': 'primary-geometry'
     };
-    // mark fid as primary key
-    properties['fid']['x-ogc-role'] = 'id';
+    // mark fid or id as primary key
+    ['fid', 'id'].forEach((key) => {
+      if (properties[key]) {
+        properties[key]['x-ogc-role'] = 'id';
+      }
+    });
 
     return {
       properties: properties
